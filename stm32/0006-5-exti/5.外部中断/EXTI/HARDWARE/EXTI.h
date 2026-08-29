d650c80d04e496361c51e1bc8d3931cd78917931
Elinsa Enos <1092903042@qq.com>
2026-08-29 21:35:08 +0800

    chore: remove remaining consumerless source trees

--- stm32/0006-5-exti/5.外部中断/EXTI/HARDWARE/EXTI.h
+++
@@ -1,7 +0,0 @@
-#ifndef __EXTI_H 
-#define __EXTI_H 
-#include "stm32f10x_exti.h" 
-
-void EXTI5_Init(void); 
-
-#endif 
--- stm32/0036-bluetoothapp/BlueToothAPP/WHEELTEC_BSP/Inc/bsp_usart.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BSP_USART_H
-#define __BSP_USART_H
-
-#include "sys.h"
-
-void uart1_init(u32 bound);
-
-#endif
