07a159d4ffb67165c905bcf62e46cb95ae32baae
Elinsa Enos <1092903042@qq.com>
2026-08-29 19:06:39 +0800

    refactor: remove unselected R680 firmware roots

--- r680/0064-bt04-bluetooth-resources/BT04蓝牙模块资料/MiniBalanceV1.0 APP源码/src/com/example/android/BluetoothChat/OnFingerListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.android.BluetoothChat;
-
-public interface OnFingerListener {
-	int inter = 0;
-	public void onClick(myView view);
-	public void onClick(myView view,int which);
-}
--- r680/0064-bt04-bluetooth-resources/BT04蓝牙模块资料/MiniBalanceV1.0 APP源码/src/com/example/android/BluetoothChat/OnRockerListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.android.BluetoothChat;
-
-public interface OnRockerListener {
-	public void onRocker(int which);
-	public void onRocker(MySurfaceView mySurfaceView,int which);
-}
